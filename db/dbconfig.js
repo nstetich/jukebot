931677610893c7a815da81f3ed4fb018ae9b0635
Nick Stetich <nstetich@gmail.com>
2015-03-28 08:17:35 -0700

    Add database with user table

---
+++ db/dbconfig.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+var connectionInfo = {
+  user: 'jukebot',
+  password: 'Tun3z',
+  database: 'jukebot'
+};
+
+if (process.env.VCAP_SERVICES) {
+  var services = JSON.parse(process.env.VCAP_SERVICES);
+  var mysqlConfig = services["p-mysql"];
+  if (mysqlConfig) {
+    var node = mysqlConfig[0];
+    connectionInfo = {
+        host: node.credentials.hostname,
+        port: node.credentials.port,
+        user: node.credentials.username,
+        password: node.credentials.password,
+        database: node.credentials.name
+    };
+  }
+}
+
+exports.query = function(query, callback) {
+  var connection = mysql.createConnection(connectionInfo);
+  connection.query(query, function(queryError, result) {
+    callback(queryError, result);
+  });
+  connection.end();
+};
