05777dc823df38e2691c4c3767ade36f621f1343
Nick Stetich <nstetich@gmail.com>
2015-04-04 08:38:20 -0700

    Add creation of random api client id and secret

--- model/client.js
+++ model/client.js
@@ -1,15 +1,23 @@
 var _ = require('lodash');
 var db = require('../db/dbconfig');
+var crypto = require('crypto');
+var _ = require('lodash');
+
+function randBytes(n) {
+  return new Buffer(crypto.randomBytes(n)).toString('hex');
+}
 
 var Client = db.bookshelf.Model.extend({
   tableName: 'clients',
 
   initialize: function() {
     this.on('creating', function() {
-      // console.log("creating. " + JSON.stringify(arguments));
-    });
-    this.on('saving', function() {
-      // console.log("saving. " + JSON.stringify(arguments));
+      if (!this.get('api_client_id')) {
+        this.set("api_client_id", randBytes(10));
+      }
+      if (!this.get("api_client_secret")) {
+        this.set("api_client_secret", randBytes(20));
+      }
     });
   }
 });
--- test/test-client.js
+++ test/test-client.js
@@ -10,6 +10,7 @@ describe('Client', function () {
     var knexfile = require('../db/knexfile');
     knex = require('knex')(knexfile.unit);
     bookshelf = require('bookshelf')(knex);
+
     Client = proxyquire('../model/client', {
       '../db/dbconfig': {
         knex: knex,
@@ -26,7 +27,9 @@ describe('Client', function () {
     return knex('clients').delete();
   });
 
-  it("should have a Client", function () {
+  it("should have valid test setup", function () {
+    assert.ok(knex);
+    assert.ok(bookshelf);
     assert.ok(Client);
   })
 
@@ -40,9 +43,68 @@ describe('Client', function () {
       return new Client({api_client_id: 'clientId'}).fetch()
     }).then(function (model) {
       assert.ok(model);
-      assert.equal(client.get("name"), "Hello Client");
-      assert.equal(client.get("api_client_id"), 'clientId');
-      assert.equal(client.get("api_client_secret"), "secret");
+      assert.equal(model.get("name"), "Hello Client");
+      assert.equal(model.get("api_client_id"), 'clientId');
+      assert.equal(model.get("api_client_secret"), "secret");
+    });
+  });
+
+  it("should add default api_client_id for a new client", function () {
+    function assertRandomHex(val, len) {
+      assert.match(val, /^[0-9a-f]+$/);
+      assert.lengthOf(val, len);
+    }
+    var client = new Client({
+      name: "Newbie Client",
+      api_client_secret: "s3Kr3t!"
+    })
+    return client.save().then(function (model) {
+      assertRandomHex(model.get('api_client_id'), 20);
+      assert.equal(model.get('api_client_secret'), 's3Kr3t!');
+    });
+  });
+
+  it("should add default api_client_secret for a new client", function () {
+    function assertRandomHex(val, len) {
+      assert.match(val, /^[0-9a-f]+$/);
+      assert.lengthOf(val, len);
+    }
+    var client = new Client({
+      name: "Newbie Client",
+      api_client_id: "neeeewbie"
+    })
+    return client.save().then(function (model) {
+      assert.equal(model.get('api_client_id'), 'neeeewbie');
+      assertRandomHex(model.get('api_client_secret'), 40);
+    });
+  });
+
+  it ("should not reuse api_client_id and api_client_secret", function () {
+    var id, secret;
+    return new Client({name: "First Client"}).save().then(function (first) {
+      id = first.get('api_client_id');
+      secret = first.get('api_client_secret');
+      return new Client({name: "Second Client"}).save();
+    }).then(function (second) {
+      assert.notEqual(second.get('api_client_id'), id);
+      assert.notEqual(second.get('api_client_secret'), secret);
+    });
+  })
+
+  it("should not overwrite api_client_id and api_client_secret for existing client", function () {
+    var id, secret;
+    var client = new Client({
+      name: "Oldbie Client"
+    })
+    return client.save().then(function (model) {
+      id = model.get('api_client_id');
+      secret = model.get('api_client_secret');
+      client.set("name", "same client, new name");
+      return client.save();
+    }).then(function (model) {
+      assert.equal(model.get("name"), "same client, new name");
+      assert.equal(model.get("api_client_id"), id);
+      assert.equal(model.get("api_client_secret"), secret);
     });
   });
 
