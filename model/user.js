3b710cec1abf8e14b096808a778c2f03501f9608
Nick Stetich <nstetich@gmail.com>
2015-04-04 08:55:16 -0700

    Remove code that was moved to knex migrations

--- model/user.js
+++ model/user.js
@@ -2,21 +2,6 @@ var db = require('../db/dbconfig');
 var knex = db.knex;
 var _ = require('lodash');
 
-console.log("Checking for table 'users'");
-knex.schema.hasTable('users').then(function (exists) {
-  if (!exists) {
-    console.log("Creating table 'users'.")
-    return knex.schema.createTable('users', function (table) {
-      table.integer('github_id').primary();
-      table.string('github_username');
-      table.string('avatar_url');
-    });
-    return create;
-  }
-}).catch(function (err) {
-  console.log("Error creating table: " + JSON.stringify(err));
-});
-
 exports.authenticateUser = function(accessToken, accessTokenSecret, githubUserData, promise) {
   var authData = {
     accessToken: accessToken,
@@ -35,7 +20,9 @@ exports.authenticateUser = function(accessToken, accessTokenSecret, githubUserDa
     }).then(function (result) {
       promise.fulfill(authData);
     }).catch(function (err) {
-      console.log("Error retrieving/persisting user.");
+      var msg = "Error retrieving or persisting user. " + JSON.stringify(err);
+      console.log(msg);
+      promise.fulfill(msg);
     });
   return promise;
 };
