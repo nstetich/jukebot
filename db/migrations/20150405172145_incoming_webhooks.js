eb1028f8053c5d4881d6a4bb39547f6a5fee31fb
Nick Stetich <nstetich@gmail.com>
2015-04-23 18:52:47 -0700

    Switch to one migration script

--- db/migrations/20150405172145_incoming_webhooks.js
+++ db/migrations/20150405172145_incoming_webhooks.js
@@ -1,14 +1,42 @@
-'use strict';
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('slack_incoming_webhooks', function (table) {
+  return knex.schema.createTable('clients', function (table) {
     table.increments('id').primary();
-    table.integer('client_id').unsigned()
-      .references('id').inTable('clients').notNullable();
-    table.string('url', 255);
+    table.string('name');
+    table.string('api_client_id', 20).unique().index();
+    table.string('api_client_secret', 40);
+  }).then(function () {
+    return knex.schema.createTable('users', function (table) {
+      table.integer('github_id').primary();
+      table.string('github_username');
+      table.string('avatar_url');
+    });
+  }).then(function () {
+    return knex.schema.createTable('client_tokens', function (table) {
+      table.increments('id').primary();
+      table.integer('client_id').unsigned()
+        .references('id').inTable('clients').notNullable();
+      table.string('api_client_token', 40).index();
+      table.dateTime('created');
+      table.dateTime('expires');
+    });
+  }).then(function () {
+    return knex.schema.createTable('slack_incoming_webhooks', function (table) {
+      table.increments('id').primary();
+      table.integer('client_id').unsigned()
+        .references('id').inTable('clients').notNullable();
+      table.string('url', 255);
+    });
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('slack_incoming_webhooks');
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    Promise.all([
+      knex.schema.dropTable('client_tokens'),
+      knex.schema.dropTable('slack_incoming_webhooks')
+    ]).then(function () {
+      return knex.schema.dropTable('clients');
+    })
+  ]);
 };
